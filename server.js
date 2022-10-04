a77e649c15b20827e22205960884fe7f7f8bc869
pamungkas <ridwan.pamungks@gmail.com>
2022-10-04 19:26:14 +0700

    set port in env

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const express = require('express')
 const routes = require('./routes')
 
 const app = express()
-const port = 3000
+// set port
+const port = process.env.APP_PORT || 3000
 
 app.use('/', routes)
 
