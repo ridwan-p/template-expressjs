ab93ce9fa5a04259b1911743edaf50c6e2fcbfd4
pamungkas <ridwan.pamungks@gmail.com>
2022-10-04 18:15:38 +0700

    updated controller style

--- app/controller/HomeController.js
+++ app/controller/HomeController.js
@@ -3,5 +3,7 @@ const User = require("../models/User")
 const index = (req, res, next) => {
   return res.send('Hi, is there anything I can help you with?')
 }
-
-module.exports = { index }
+const HomeController = {
+  index
+}
+module.exports = HomeController
