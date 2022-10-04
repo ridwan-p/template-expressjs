21d9afa4a93bee4a98d651d9be1cff4914e5feb0
pamungkas <ridwan.pamungks@gmail.com>
2022-10-04 18:32:05 +0700

    wording home

--- app/controller/HomeController.js
+++ app/controller/HomeController.js
@@ -1,7 +1,7 @@
 const User = require("../models/User")
 
 const index = (req, res, next) => {
-  return res.send('Hi, is there anything I can help you with?')
+  return res.send('Welcome to my apps')
 }
 const HomeController = {
   index
